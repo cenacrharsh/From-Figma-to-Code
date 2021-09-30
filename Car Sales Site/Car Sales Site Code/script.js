e38c7522962aa4bd8f69886dff2b881df1c9da7c
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-09-30 14:13:22 +0530

    added functionality to filter section

---
+++ Car Sales Site/Car Sales Site Code/script.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector(".menu");
+
+const panel = document.querySelector(".filter");
+
+const panelStyles = window.getComputedStyle(panel);
+
+// Js doesn't have access to styles applied to HTML elements through CSS files so we use getComputedStyles
+
+menuBtn.addEventListener("click", () => {
+  if (panelStyles.getPropertyValue("clip-path") === "circle(0% at 100% 0%)") {
+    panel.style.clipPath = "circle(100% at 50% 50%)";
+  } else {
+    panel.style.clipPath = "circle(0% at 100% 0%)";
+  }
+});
+
+document.addEventListener("mouseup", (event) => {
+  // Nothing should happen if we click somewhere on the panel itself
+  if (panel.style.getPropertyValue("clip-path") === "circle(100% at 50% 50%)") {
+    if (!panel.contains(event.target)) {
+      panel.style.clipPath = "circle(0% at 100% 0%)";
+    }
+  }
+});
